08d8ce24f54e786fe3dff0bc3b57a90d60b1df28
Paul Kirth <pk1574@gmail.com>
2016-11-22 00:45:55 -0800

    try new setup_ubuntu scripts

--- build_cross_compiler.sh
+++ build_cross_compiler.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f hypervisor/.cross_compiler_created  ] ; then
     cd hypervisor
-    CROSS_COMPILER=gcc_520 ./tools/scripts/create_cross_compiler.sh
+    ./tools/scripts/setup_ubuntu.sh --use_llvm_clang --compiler gcc_520 --local_compiler
+    #CROSS_COMPILER=gcc_520 ./tools/scripts/create_cross_compiler.sh
     touch .cross_compiler_created
 fi
