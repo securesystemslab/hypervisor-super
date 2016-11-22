fd4336ca45a84e5da7bb4f073fcec554be68fc15
Paul Kirth <pk1574@gmail.com>
2016-11-21 19:30:33 -0800

    fix build file checked

--- build_cross_compiler.sh
+++ build_cross_compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f hypervisor/.hypervisor_configured  ] ; then
+if [ ! -f hypervisor/.cross_compiler_created  ] ; then
     cd hypervisor
     CROSS_COMPILER=gcc_520 ./tools/scripts/create_cross_compiler.sh
     touch .cross_compiler_created
