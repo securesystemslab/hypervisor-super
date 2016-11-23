ccb2b586b257b22f72f89a17727e7212101a0192
Paul Kirth <pk1574@gmail.com>
2016-11-23 01:11:01 -0800

    update scripts

--- build_cross_compiler.sh
+++ build_cross_compiler.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f hypervisor/.cross_compiler_created  ] ; then
     cd hypervisor
-    ./tools/scripts/setup_ubuntu.sh --use_llvm_clang --compiler gcc_520 --local_compilers
+    ./tools/scripts/setup_ubuntu.sh --compiler clang_38 --local_compilers
     #CROSS_COMPILER=gcc_520 ./tools/scripts/create_cross_compiler.sh
     touch .cross_compiler_created
 fi
