b3a7be2bb3e4c897f256eb460a074eafc564e445
Paul Kirth <pk1574@gmail.com>
2016-12-10 14:16:19 -0800

    fix whitespace

--- scripts/build_gold.sh
+++ scripts/build_gold.sh
@@ -15,7 +15,7 @@ mkdir -p /tmp/bareflank/build_binutils
 pushd /tmp/bareflank/build_binutils
 
 
-../src_binutils/configure --enable-gold --enable-plugins  --target=x86_64-elf --prefix="$PREFIX" --disable-nls --disable-werror --with-sysroot
+../src_binutils/configure --enable-gold --enable-plugins --target=x86_64-elf --prefix="$PREFIX" --disable-nls --disable-werror --with-sysroot
 make -j2
 make -j2 install
 
