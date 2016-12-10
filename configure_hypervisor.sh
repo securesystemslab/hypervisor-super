e519de0890c73290d0dc5b7703c5d5d4dda41d77
Paul Kirth <pk1574@gmail.com>
2016-12-10 11:33:57 -0800

    update configure command

--- configure_hypervisor.sh
+++ configure_hypervisor.sh
@@ -3,6 +3,7 @@
 
 if [ ! -f hypervisor/.hypervisor_configured  ] ; then
     cd hypervisor
+    ./configure --compiler clang_38 --local-compilers
     #./configure --use_llvm_clang --compiler gcc_520
     #echo export LOCAL_COMPILER=\"true\"
     touch .hypervisor_configured
