81b4b6adf65499679bbb989ee083f71f125f2876
Paul Kirth <pk1574@gmail.com>
2017-03-20 20:27:23 -0700

    add support for hyperkernel

--- configure_hypervisor.sh
+++ configure_hypervisor.sh
@@ -4,6 +4,6 @@ COMPILER_PATH=$HOME/compilers/clang_38/bin
 
 if [ ! -f hypervisor/.hypervisor_configured  ] ; then
     cd hypervisor
-    ./configure --compiler $COMPILER_PATH/clang --linker $COMPILER_PATH/x86_64-elf-ld
+    ./configure --compiler $COMPILER_PATH/clang --linker $COMPILER_PATH/x86_64-elf-ld -m hyperkernel/bin/hyperkernel.modules
     touch .hypervisor_configured
 fi
