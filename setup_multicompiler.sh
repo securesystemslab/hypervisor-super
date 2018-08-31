99e321bea0a87d7d0c78c06d719b4b499f1fa1d5
Matt Dees <mdees@uci.edu>
2018-08-30 18:05:04 -0700

    setup_multicompiler.sh: Added check for existence of multicompiler directory

--- setup_multicompiler.sh
+++ setup_multicompiler.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-git clone -b ubuntu18_04 git@github.com:/securesystemslab/multicompiler-automation.git multicompiler
+if [ ! -d multicompiler ]; then
+    git clone -b ubuntu18_04 git@github.com:/securesystemslab/multicompiler-automation.git multicompiler
+fi
  
 pushd multicompiler
 ./build.sh
