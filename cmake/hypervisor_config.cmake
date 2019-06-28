cdd0c01daa2ac7468824b27ed79862e69104c556
Matt Dees <mdees@uci.edu>
2019-06-28 21:06:23 +0000

    Removed C++14 requirement since multicompiler was updated to LLVM 7.0

--- cmake/hypervisor_config.cmake
+++ cmake/hypervisor_config.cmake
@@ -1,26 +1,20 @@
-# Turn on unittests
-set(ENABLE_BUILD_TEST ON)
-
 # Set release build for x86 architecture
 set(BUILD_TYPE Release)
 set(BUILD_TARGET_ARCH x86_64)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(ENABLE_BUILD_TEST ON)
 
-# Set flags to use C++14 because UCI Multicompiler is currently based
-# on a clang version that does not support C++17.
-unset(BFFLAGS_VMM_CXX)
 
-list(APPEND BFFLAGS_VMM_CXX
-    -x c++
-    -std=c++14
-)
+# Uncomment these lines out to only build VMM components
+# set(ENABLE_BUILD_EFI OFF)
+# set(ENABLE_BUILD_USERSPACE OFF)
+
 
 # Additional flags used for building the VMM components should be set to the
 # desired randomization flags.
 list (APPEND C_FLAGS_VMM
     ${MULTICOMPILER_C_FLAGS}
 )
-
 list (APPEND CXX_FLAGS_VMM
     ${MULTICOMPILER_CXX_FLAGS}
 )
