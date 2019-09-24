c8f4eb726f8495e1389b7b3a9047954f6f468814
Matt Dees <mdees@uci.edu>
2019-09-24 20:29:43 +0000

    [SafeStack] Updated build script and hypervisor CMake toolchain files for building the SafeStack run-time library.

--- cmake/hypervisor_config.cmake
+++ cmake/hypervisor_config.cmake
@@ -3,10 +3,10 @@ set(BUILD_TYPE Release)
 set(BUILD_TARGET_ARCH x86_64)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-set(ENABLE_BUILD_TEST OFF)
+set(ENABLE_BUILD_TEST ON)
 
 # Uncomment these lines out to only build VMM components
-set(ENABLE_BUILD_USERSPACE OFF)
+set(ENABLE_BUILD_USERSPACE ON)
 
 
 # Additional flags used for building the VMM components should be set to the
