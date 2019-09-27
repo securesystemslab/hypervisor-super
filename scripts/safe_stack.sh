cf2d04f6b1fb7b907028456df8960987c541cbbe
Matt Dees <mdees@uci.edu>
2019-09-27 20:20:32 +0000

    -fsanitize=safe-stack now supported

--- scripts/safe_stack.sh
+++ scripts/safe_stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 MC_CFLAGS="-fsanitize=safe-stack"
 MC_CXX_FLAGS=${MC_CFLAGS}
-MC_LD_FLAGS="/srv/bfbuild/hypervisor-super/multicompiler/llvm/build-compiler-rt/lib/linux/libclang_rt.safestack-x86_64.a -u __safestack_init"
+MC_LD_FLAGS="-u __safestack_init"
 pushd ..
 MULTICOMPILER_C_FLAGS="${MC_CFLAGS}" MULTICOMPILER_CXX_FLAGS="${MC_CXX_FLAGS}" \
 MULTICOMPILER_LD_FLAGS="${MC_LD_FLAGS}" make hypervisor_using_multicompiler
