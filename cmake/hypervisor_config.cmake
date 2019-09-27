3b361ceffb6c6663beaed83d276c7782b7a774a4
Matt Dees <mdees@uci.edu>
2019-09-27 20:17:52 +0000

    Added MULTICOMPILER_LLVM_PATH variable for use in libcxx[abi] builds

--- cmake/hypervisor_config.cmake
+++ cmake/hypervisor_config.cmake
@@ -2,11 +2,14 @@
 set(BUILD_TYPE Release)
 set(BUILD_TARGET_ARCH x86_64)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-set(ENABLE_BUILD_TEST ON)
+set(BUILD_SHARED_LIBS OFF)
+set(BUILD_STATIC_LIBS ON)
+set(SAFESTACK_RUNTIME_LIB ${CMAKE_CURRENT_LIST_DIR}/../multicompiler/tools/lib/libclang_rt.safestack-x86_64.a)
+set(MULTICOMPILER_LLVM_PATH ${CMAKE_CURRENT_LIST_DIR}/../multicompiler/llvm)
+set(ENABLE_BUILD_TEST OFF)
 
 # Uncomment these lines out to only build VMM components
-set(ENABLE_BUILD_USERSPACE ON)
+set(ENABLE_BUILD_USERSPACE OFF)
 
 
 # Additional flags used for building the VMM components should be set to the
