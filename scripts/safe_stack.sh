7e9aa298f543ffca06f124d036e6df542ca56101
Matt Dees <mdees@uci.edu>
2019-12-04 11:14:40 -0800

    Added DEFAULT_VMM parameter to hypervisor build to point to evaluation hypervisor

--- scripts/safe_stack.sh
+++ scripts/safe_stack.sh
@@ -2,8 +2,9 @@
 MC_CFLAGS="-fsanitize=safe-stack"
 MC_CXX_FLAGS=${MC_CFLAGS}
 MC_LD_FLAGS="-u __safestack_init"
-pushd ..
+
+pushd "$( dirname "${BASH_SOURCE[0]}" )"/..
 MULTICOMPILER_C_FLAGS="${MC_CFLAGS}" MULTICOMPILER_CXX_FLAGS="${MC_CXX_FLAGS}" \
-	MULTICOMPILER_LD_FLAGS="${MC_LD_FLAGS}" MULTICOMPILER_CMAKE_FLAGS=" -DSAFESTACK_RUNTIME_LIB=$(pwd)/multicompiler/llvm/projects/compiler-rt/build/lib/linux/libclang_rt.safestack-x86_64.a " make hypervisor_using_multicompiler
+	MULTICOMPILER_LD_FLAGS="${MC_LD_FLAGS}" MULTICOMPILER_CMAKE_FLAGS=" -DSAFESTACK_RUNTIME_LIB=$(pwd)/multicompiler/llvm/projects/compiler-rt/build/lib/linux/libclang_rt.safestack-x86_64.a -DDEFAULT_VMM=safestackeval" make hypervisor_using_multicompiler
 popd
 
